5fa112dd6aef4fe7412c1a5b637f86c4b7f05491
youkero <ryszardjanicki1@gmail.com>
2020-03-21 16:10:51 +0100

    unicafe step 1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Header = () => <h1>give feedback</h1>;
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+const Statistics = ({ good, neutral, bad }) => (
+  <div>
+    <h2>statistics</h2>
+    <p>good {good}<br />neutral {neutral}<br />bad {bad}</p>
+  </div>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Header />
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
