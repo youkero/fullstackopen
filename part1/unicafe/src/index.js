a66dfbc26bc386dc897c05ea5ee4c268df94efa2
youkero <ryszardjanicki1@gmail.com>
2020-03-21 18:41:20 +0100

    unicafe step 6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom'
 
 const Header = ({ text }) => <h1>{text}</h1>;
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
+const Statistic = ({ text, value }) => <tr><td>{text}</td> <td>{value}</td></tr>;
 
-const Statistic = ({ text, value }) => <div>{text} {value}</div>
+const StatsTable = ({ stats }) => (
+  <table>
+    <tbody>
+      <Statistic text={stats[0].text} value={stats[0].value} />
+      <Statistic text={stats[1].text} value={stats[1].value} />
+      <Statistic text={stats[2].text} value={stats[2].value} />
+      <Statistic text={stats[3].text} value={stats[3].value} />
+      <Statistic text={stats[4].text} value={stats[4].value} />
+      <Statistic text={stats[5].text} value={stats[5].value} />
+    </tbody>
+  </table>
+);
 
 const App = () => {
   // save clicks of each button to own state
@@ -30,6 +42,15 @@ const App = () => {
   const average = (good + -bad) / all;
   const positive = good * 100 / all;
 
+  const stats = [
+    { text: "good", value: good },
+    { text: "neutral", value: neutral },
+    { text: "bad", value: bad },
+    { text: "all", value: all },
+    { text: "average", value: average ? average : 0 },
+    { text: "positive", value: (positive ? positive : 0) + ' %' },
+  ];
+
   return (
     <div>
       <Header text='give feedback' />
@@ -37,12 +58,7 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
       <Header text='statistics' />
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={all} />
-      <Statistic text="average" value={average ? average : 0} />
-      <Statistic text="positive" value={(positive ? positive : 0) + ' %'} />
+      <StatsTable stats={stats} />
     </div>
   );
 }
