e0b85717e9f94ef420487c8696e4594242ab23a9
youkero <ryszardjanicki1@gmail.com>
2020-03-21 17:30:04 +0100

    unicafe step 5

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,33 +1,10 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = () => <h1>give feedback</h1>;
+const Header = ({ text }) => <h1>{text}</h1>;
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>;
-const Statistics = ({ good, neutral, bad }) => {
-  const all = good + neutral + bad;
 
-  if (!all) return (
-    <div>
-      <h2>statistics</h2>
-      <p>No feedback given</p>
-    </div>
-  );
-
-  const average = (good + -bad) / all;
-  const positive = good * 100 / all;
-
-  return (
-    <div>
-      <h2>statistics</h2>
-      good {good} <br />
-      neutral {neutral} <br />
-      bad {bad} <br />
-      all {all} <br />
-      average {average ? average : 0} <br />
-      positive {positive ? positive : 0} % <br />
-    </div>
-  );
-};
+const Statistic = ({ text, value }) => <div>{text} {value}</div>
 
 const App = () => {
   // save clicks of each button to own state
@@ -35,13 +12,37 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const all = good + neutral + bad;
+
+  if (!all) {
+    return (
+      <div>
+        <Header text='give feedback' />
+        <Button handleClick={() => setGood(good + 1)} text='good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+        <Button handleClick={() => setBad(bad + 1)} text='bad' />
+        <Header text='statistics' />
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+
+  const average = (good + -bad) / all;
+  const positive = good * 100 / all;
+
   return (
     <div>
-      <Header />
+      <Header text='give feedback' />
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Header text='statistics' />
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={average ? average : 0} />
+      <Statistic text="positive" value={(positive ? positive : 0) + ' %'} />
     </div>
   );
 }
