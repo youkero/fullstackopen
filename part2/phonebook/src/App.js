86c82e2c1f1e26a1c209561f71bc9dce1ff851e8
youkero <ryszardjanicki1@gmail.com>
2020-03-26 21:08:48 +0100

    initail persons from db.json

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Person = ({ name, number }) => (<div>{name} {number}</div>);
-const Persons = ({ persons, filter }) => (
+const Persons = ({ persons }) => (
   <>
     {persons.map((person) => <Person key={person.name} name={person.name} number={person.number} />)}
   </>
@@ -24,13 +25,7 @@ const PersonForm = ({ addNewPerson, newName, onNameChange, newNumber, onNumberCh
 const Filter = ({ newFilter, onFilter }) => <div>filter shown with <input value={newFilter} onChange={onFilter} /></div>;
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]);
-
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newFilter, setNewFilter] = useState('');
@@ -67,6 +62,14 @@ const App = () => {
   }
 
   const filteredPersons = persons.filter((person) => person.name.toLowerCase().includes(newFilter.toLowerCase()));
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+      })
+  }, [])
 
   return (
     <div>
