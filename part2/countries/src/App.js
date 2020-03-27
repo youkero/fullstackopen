52e36fa32ce8585f0c1d68693884e0d1fc76b747
youkero <ryszardjanicki1@gmail.com>
2020-03-27 19:50:21 +0100

    add show button

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
-const Country = ({ name }) => (
-  <div>{name}</div>
+const Country = ({ name, onShow }) => (
+  <div>{name} <button type="button" value={name} onClick={onShow}>show</button></div>
 );
 
-const Countries = ({ countries }) => {
+const Countries = ({ countries, onShow }) => {
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>
   } else if (countries.length === 1) {
@@ -16,16 +16,16 @@ const Countries = ({ countries }) => {
         <div>population {countries[0].population}</div>
         <h4>languages</h4>
         <ul>
-          {countries[0].languages.map((language) => <li>{language.name}</li>)}
+          {countries[0].languages.map((language) => <li key={language.name}>{language.name}</li>)}
         </ul>
-        <img src={countries[0].flag} width='100' />
+        <img src={countries[0].flag} width='100' alt={countries[0].name + " flag"} />
       </div>
     )
   }
 
   return (
     <div>
-      {countries.map((country) => <Country key={country.name} name={country.name} />)}
+      {countries.map((country) => <Country key={country.name} name={country.name} onShow={onShow} />)}
     </div>
   );
 }
@@ -38,6 +38,8 @@ const App = () => {
     setFilter(event.target.value);
   }
 
+  const onShow = (event) => setFilter(event.target.value);
+
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
@@ -49,7 +51,7 @@ const App = () => {
   return (
     <div>
       <div>find countries <input value={filter} onChange={onFilter} /></div>
-      <Countries countries={filteredCountries} />
+      <Countries countries={filteredCountries} onShow={onShow} />
     </div>
   )
 }
